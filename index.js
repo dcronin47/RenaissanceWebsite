1cf8b978cec27225ac1aa58531fa7d124fe1e9d0
dcronin47 <dcronin47@gmail.com>
2021-04-25 15:23:42 -0400

    added all pages, images, buttons, structure

---
+++ index.js
@@ -0,0 +1,27 @@
+function loadButtons() {
+    $("#buttons").append(`
+        <div content="column">
+            <input class="button" id="arch" value="Architecture" onclick="location.href='architecture.html'"/>>
+        </div>
+        <div content="column">
+            <input class="button" id="masc" value="Masculinity" onclick="location.href='masculinity.html'">
+        </div>
+        <div content="column">
+            <input class="button" id="femi" value="Femininity" onclick="location.href='Femininity.html'">
+        </div>
+        <div content="column">
+            <input class="button" id="wors" value="Worship" onclick="location.href='worship.html'">
+        </div>
+        <div content="column">
+            <input class="button" id="powe" value="Worldly Power" onclick="location.href='power.html'">
+        </div>
+        <div content="column">
+            <input class="button" id="othe" value="The Other" onclick="location.href='other.html'">
+        </div>
+        <div content="column">
+            <input class="button" id="inti" value="Intimacy" onclick="location.href='romance.html'">
+        </div>
+        <div content="column">
+            <input class="button" id="past" value="Past and Future" onclick="location.href='pastfuture.html'">
+        </div>`)
+}
